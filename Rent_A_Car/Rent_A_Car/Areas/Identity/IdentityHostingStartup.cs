c5493430ff146d65fd6337befb1f767256e111b7
Pavel Panev <panev62@gmail.com>
2022-04-03 19:33:37 +0300

    dosta raboti

---
+++ Rent_A_Car/Rent_A_Car/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rent_A_Car.Data;
+
+[assembly: HostingStartup(typeof(Rent_A_Car.Areas.Identity.IdentityHostingStartup))]
+namespace Rent_A_Car.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Rent_A_Car/Rent_A_Car/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Rent_A_Car.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string Password { get; set; }
+        public string Egn { get; set; }
+        public string UserPhone { get; set; }
+        public string Emaill { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+        public byte[] ProfilePicture { get; set; }
+    }
+    
+    
+}
